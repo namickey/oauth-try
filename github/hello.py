8beacba3e3bbc462c7dbfe62669db83c80c8cf28
namickey <teethdai@gmail.com>
2019-09-02 21:17:39 +0900

    fix

--- github/hello.py
+++ github/hello.py
@@ -5,6 +5,8 @@ import logging
 from flask import current_app, Flask, redirect, request, session, url_for, render_template
 import httplib2
 import urllib
+import hashlib
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -19,14 +21,19 @@ def top():
 
 @app.route('/login')
 def login():
-    id=app.config['client_id']
+    client_id=app.config['client_id']
     url='https://github.com/login/oauth/authorize'
+    redirect_uri = 'http://localhost:8080/oauthgithub'
+    state = json.dumps({
+        'random_key': hashlib.sha256(os.urandom(1024)).hexdigest(),
+        'return_url': redirect_uri
+    })
+    session['state'] = state
     scope = 'user:email%20repo'
-    return redirect(url + '?client_id='+id+'&state=aabbcc&scope='+scope+'&redirect_uri=http://localhost:8080/oauthgithub')
+    return redirect(url + '?client_id='+client_id+'&state='+state+'&scope='+scope+'&redirect_uri='+redirect_uri)
 
 @app.route('/logout')
 def logout():
-    del session['profile']
     del session['login']
     del session['token']
     session.modified = True
@@ -34,42 +41,41 @@ def logout():
 
 @app.route('/oauthgithub')
 def oauthgithub():
-    code = request.args.get('code')
-    state = request.args['state'].encode('utf-8')
-    http = httplib2.Http()
-    body = {'client_id': app.config['client_id'], 'client_secret':app.config['client_secret'], 'code':code}
-    resp, content = http.request("https://github.com/login/oauth/access_token",
-                       method="POST",
-                       headers={'Content-type': 'application/x-www-form-urlencoded'},
-                       body=urllib.parse.urlencode(body))
-    token = str(content).split('&')[0].split('=')[1]
-    session['token'] = token
-    resp, content = http.request('https://api.github.com/user',
-                        headers={'Authorization': 'token ' + token})
-    aa = json.loads(content.decode('utf-8'))
-    session['login'] = aa['login']
-    session['profile'] = 'aa'
+    state = request.args['state']
+    if 'state' in session and session['state'] == state:
+        code = request.args.get('code')
+        http = httplib2.Http()
+        body = {'client_id': app.config['client_id'], 'client_secret':app.config['client_secret'], 'code':code}
+        resp, content = http.request("https://github.com/login/oauth/access_token",
+                           method="POST",
+                           headers={'Content-type': 'application/x-www-form-urlencoded'},
+                           body=urllib.parse.urlencode(body))
+        session['token'] = str(content).split('&')[0].split('=')[1]
+        session['login'] = getUserInfo()['login']
+    if 'state' in session:
+        del session['state']
     return redirect('/')
 
 @app.route('/user')
 def user():
+    if not 'login' in session:
+        return redirect('/')
+    return render_template('user.html', userInfo=getUserInfo())
+
+def getUserInfo():
     http = httplib2.Http()
-    token = session['token']
-    resp, content = http.request('https://api.github.com/users/'+session['login'],
-                        headers={'Authorization': 'token ' + token})
-    userInfo = json.loads(content.decode('utf-8'))
-    return render_template('user.html', userInfo=userInfo)
+    resp, content = http.request('https://api.github.com/user',
+                        headers={'Authorization': 'token ' + session['token']})
+    return json.loads(content.decode('utf-8'))
 
 @app.route('/repo')
-def token1():
+def repo():
+    if not 'login' in session:
+        return redirect('/')
     http = httplib2.Http()
-    token = session['token']
     resp, content = http.request('https://api.github.com/users/'+session['login']+'/repos',
-                        headers={'Authorization': 'token ' + token})
+                        headers={'Authorization': 'token ' + session['token']})
     repoList = json.loads(content.decode('utf-8'))
-    #repos = []
-    #for x in repoList:
-    #    repos.append(x['name'])
     return render_template('repo.html', repos=[x['name'] for x in repoList])
 
 if __name__ == '__main__':
